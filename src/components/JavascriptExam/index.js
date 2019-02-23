669f0f5be6b70347c5958ede82046bbed64691b9
Jomel <jomel.inovejas@gmail.com>
2019-02-23 19:24:37 +0800

    Prototyping UI for questions

--- src/components/JavascriptExam/index.js
+++ src/components/JavascriptExam/index.js
@@ -1,16 +1,63 @@
 import React, { Component } from "react";
 import { compose } from "recompose";
 
-import { Container } from "reactstrap";
+import {
+    Container,
+    Row,
+    Col,
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    Label,
+    Input
+} from "reactstrap";
 
 import { withAuthorization } from "../Session";
 import { withFirebase } from "../Firebase";
+
+const INITIAL_STATE = {
+    mcquestions: [
+        {
+            quid: "1",
+            index: "1",
+            questionText: "What is life?",
+            options: ["option1", "option2", "option3", "option4", "option5"],
+            answer: "",
+            duration: 180
+        },
+        {
+            uid: "2",
+            index: "2",
+            questionText: "What is zero?",
+            options: ["option1", "option2", "option3", "option4", "option5"],
+            answer: "",
+            duration: 180
+        }
+    ],
+    progchallenges: [
+        {
+            uid: "3",
+            index: "3",
+            challenge: "question11",
+            answer: ""
+        },
+        {
+            uid: "4",
+            index: "4",
+            challenge: "question21",
+            answer: ""
+        }
+    ],
+    currentQuestionIndex: -1
+};
 
 class JavascriptExamPage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            ...INITIAL_STATE,
             loading: true
         };
     }
@@ -33,12 +80,61 @@ class JavascriptExamPage extends Component {
     }
 
     componentWillUnmount() {
-        this.unsubscribe();
+        // this.unsubscribe();
+    }
+
+    submitAnswer() {
+        console.log("here", this.state);
+    }
+
+    renderMCQuestions() {
+        const { mcquestions } = this.state;
+        const cardsOfQuestions = mcquestions.map(question => {
+            const { uid, options } = question;
+            const radioButtons = [1, 2, 3, 4, 5].map(option => {
+                const optionStrId = `${uid}-option${option}`;
+                return (
+                    <div>
+                        <Input type="radio" id={optionStrId} />
+                        <Label for={optionStrId}>{options[option - 1]}</Label>
+                    </div>
+                );
+            });
+
+            return (
+                <Card>
+                    <CardHeader>{question.questionText}</CardHeader>
+                    <CardBody>{radioButtons}</CardBody>
+                </Card>
+            );
+        });
+        return cardsOfQuestions;
     }
 
     render() {
-        const { loading } = this.state;
-        return <Container>Hello World {loading}</Container>;
+        /* const {
+            mcquestions,
+            progchallenges,
+            currentQuestionIndex
+        } = this.state;
+        */
+        return (
+            <Container>
+                <Row>
+                    <Col lg={{ size: 8, offset: 2 }}>
+                        {this.renderMCQuestions()}
+
+                        <Button
+                            className="mt-4"
+                            block
+                            onClick={this.submitAnswer}
+                        >
+                            Submit Answer
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        );
     }
 }
 
